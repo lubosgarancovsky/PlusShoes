a6db2bde79c8e1fcfdff374414224078b5efd2ca
Lubos1705 <lubik1181@gmail.com>
2022-08-28 23:47:37 +0200

    setup

---
+++ src/containers/header.jsx
@@ -0,0 +1,26 @@
+
+
+export function Header() {
+    return ( 
+        <header>
+            <div className="top-bar">
+                <div className="header-menu-search">
+
+                </div>
+
+                <div className="header-title">
+
+                </div>
+
+                <div className="header-button">
+
+                </div>
+            </div>
+
+            <div className="header-carousel">
+
+            </div>
+        </header>
+     );
+}
+
