5086e80d0f2228770147043fe45a3795b98c5989
Lubos1705 <lubik1181@gmail.com>
2022-08-29 23:34:50 +0200

    config base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'lubosgarancovsky.github.io/PlusShoes',
+  base: '/PlusShoes/',
   plugins: [react()]
 })
